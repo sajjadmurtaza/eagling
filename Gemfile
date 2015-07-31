ff0b921225e84fb1b813d3d5b2f53bda1155f060
Sajjad Murtaza (01770) <sajjad.murtaza@nxb.com.pk>
2015-07-31 19:23:00 +0500

    first commit

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source 'https://rubygems.org'
+
+# Specify your gem's dependencies in extra_attributes_for_all_migration.gemspec
+gemspec
---
+++ lib/extra_attributes_for_all_migration.rb
@@ -0,0 +1,5 @@
+require "extra_attributes_for_all_migration/version"
+
+module ExtraAttributesForAllMigration
+  # Your code goes here...
+end
